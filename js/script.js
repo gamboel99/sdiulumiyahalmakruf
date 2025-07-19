0dcc38c24afa99b3e596feae7752bc8e6d446648
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2025-07-19 18:26:26 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// Script JS akan ditambahkan di sini
+let index = 0;
+const slides = document.querySelectorAll(".slider img");
+
+function showNextSlide() {
+  slides.forEach((slide, i) => {
+    slide.style.opacity = (i === index) ? "1" : "0";
+  });
+  index = (index + 1) % slides.length;
+}
+
+setInterval(showNextSlide, 3000);
+window.onload = showNextSlide;
